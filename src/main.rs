5a9532819eb51656a8c9c0c29eb97301c44433d2
Jonas Nicklas <jonas.nicklas@gmail.com>
2015-01-30 16:23:54 +0100

    Rename config -> git

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,9 @@ use standard_error::StandardResult as Result;
 use std::rc::Rc;
 use std::borrow::Cow;
 
-mod config;
+mod git;
 
-static CFG: config::ConfigType = config::ConfigType::File("./partners.cfg");
+static CFG: git::ConfigType = git::ConfigType::File("./partners.cfg");
 
 // Write the Docopt usage string.
 static USAGE: &'static str = "
@@ -63,36 +63,36 @@ fn parse_author_line(config: &Rc<Config>, line: &str) -> Result<Author> {
   let mut parts = line.splitn(1, ' ');
   let nick = parts.next().unwrap().split('.').nth(1).unwrap().to_string();
   let name = parts.next().unwrap().to_string();
-  let email = config::get(&CFG, &format!("author.{}.email", nick)[]).ok();
+  let email = git::get(&CFG, &format!("author.{}.email", nick)[]).ok();
 
   Ok(Author { config: config.clone(), nick: nick, name: name, email: email })
 }
 
 fn get_authors(config: &Rc<Config>) -> Result<Vec<Author>> {
-  let lines = try!(config::list(&CFG, "author.\\w+.name"));
+  let lines = try!(git::list(&CFG, "author.\\w+.name"));
   lines.iter().map(|line| parse_author_line(config, &line[])).collect()
 }
 
 fn write_author(author: &Author) -> Result<()> {
-  try!(config::set(&CFG, &*format!("author.{}.name", author.nick), &*author.name));
+  try!(git::set(&CFG, &*format!("author.{}.name", author.nick), &*author.name));
   if let Some(ref email) = author.email {
-    try!(config::set(&CFG, &*format!("author.{}.email", author.nick), &**email));
+    try!(git::set(&CFG, &*format!("author.{}.email", author.nick), &**email));
   }
   Ok(())
 }
 
 fn print_current() {
-  println!("Name:  {}", config::get(&config::ConfigType::None, "user.name").unwrap());
-  println!("Email: {}", config::get(&config::ConfigType::None, "user.email").unwrap());
+  println!("Name:  {}", git::get(&git::ConfigType::None, "user.name").unwrap());
+  println!("Email: {}", git::get(&git::ConfigType::None, "user.email").unwrap());
 }
 
 fn main() {
   let args = Docopt::new(USAGE).and_then(|d| d.parse()).unwrap_or_else(|e| e.exit());
 
   let config = Rc::new(Config {
-    domain: config::get(&CFG, "config.domain").unwrap_or_else(|_| "example.com".to_string()),
-    prefix: config::get(&CFG, "config.prefix").unwrap_or_else(|_| "dev".to_string()),
-    separator: config::get(&CFG, "config.separator").unwrap_or_else(|_| "+".to_string()),
+    domain: git::get(&CFG, "config.domain").unwrap_or_else(|_| "example.com".to_string()),
+    prefix: git::get(&CFG, "config.prefix").unwrap_or_else(|_| "dev".to_string()),
+    separator: git::get(&CFG, "config.separator").unwrap_or_else(|_| "+".to_string()),
   });
 
   if args.get_bool("list") {
@@ -114,9 +114,9 @@ fn main() {
 
     match get_authors(&config).unwrap().iter().find(|a| a.nick == nick) {
       Some(author) => {
-        config::set(&config::ConfigType::Global, "partners.current", &*author.nick).unwrap();
-        config::set(&config::ConfigType::Global, "user.name", &*author.name).unwrap();
-        config::set(&config::ConfigType::Global, "user.email", &*author.get_email()).unwrap();
+        git::set(&git::ConfigType::Global, "partners.current", &*author.nick).unwrap();
+        git::set(&git::ConfigType::Global, "user.name", &*author.name).unwrap();
+        git::set(&git::ConfigType::Global, "user.email", &*author.get_email()).unwrap();
         print_current();
       }
       None => {
